c8423d41e8a53cf08f6e57f3f57b3b4a302f0745
Reza Zahedi <r.zahedi@gmail.com>
2025-04-18 16:47:54 -0700

    Fix conflict and solve issues

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import favicon from 'express-favicon'
 import logger from 'morgan'
-import userRouter from './routes/user'
+import router from './routes/mainRouter'
+import authRouter from './routes/auth'
 import plansRouter from './routes/plans'
 import accountRouter from './routes/account'
 import swaggerUI from 'swagger-ui-express'
 import YAML from 'yamljs'
-import authMiddleware from './middleware/authMiddleware'
-import notFoundMiddleware from './middleware/notFoundMiddleware'
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware'
+import authMiddleware from './middlewares/authMiddleware'
+import notFoundMiddleware from './middlewares/notFoundMiddleware'
 
 const app = express()
 
@@ -21,18 +21,18 @@ app.use(logger('dev'))
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/favicon.ico'))
 
-// TODO: Should setup some security middleware like: rate limiter, helmet, xss ...
+// TODO: Should setup some security middlewares like: rate limiter, helmet, xss ...
 
 // routes
+app.use('/api/v1', router)
 app.use('/api/v1/plans', plansRouter)
-app.use('/api/v1/user', userRouter)
+app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/account/plans', authMiddleware, accountRouter)
+
+app.use(notFoundMiddleware)
+// TODO: Add error handling middleware
 
 const swaggerDocument = YAML.load('./swagger.yaml')
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
-
-// error middleware
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
 
 export default app
---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express'
+import dotenv from 'dotenv'
+import UserSchema, { IUser } from '../models/Users'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import UnauthenticatedError from '../errors/unauthentication_error'
+
+dotenv.config()
+interface MyJwtPayload extends JwtPayload {
+  userId: string
+  name: string
+}
+
+const authMiddleware = async (request: Request, response: Response, next: NextFunction) => {
+  const authHeader = request.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication not provided')
+  }
+
+  const token = authHeader.split(' ')[1]
+  console.log('token', token)
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as MyJwtPayload
+    console.log('payload', payload)
+
+    // Attach the user to the authorized route
+    const user: IUser = await UserSchema.findById(payload.userId)
+      .orFail(new UnauthenticatedError('User not found'))
+      .select('-password')
+    request.user = { userId: user._id, name: user.name, email: user.email }
+    next()
+  } catch {
+    throw new UnauthenticatedError('Invalid authentication')
+  }
+}
+
+export default authMiddleware
